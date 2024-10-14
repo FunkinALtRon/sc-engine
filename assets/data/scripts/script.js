4f9091a547ee156cdb57afcf146e9ef74cc3bf2c
Alt Ron <93061518+FunkinALtRon@users.noreply.github.com>
2024-10-14 16:17:37 -0700

    added more

--- assets/data/scripts/script.js
+++
@@ -1,9 +0,0 @@
-function create() {
-    createSprite("test", 0, 0);
-    setGraphic("test", "WindowTop");
-    add("test");
-}
-
-function update(dt) {
-    sprites.test.x = Math.cos(timer * 4) * 12;
-}
